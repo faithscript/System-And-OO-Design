49c3a88422eff7fd79142cc10085de046d498c44
Faith Anyanwu <111374405+faithscript@users.noreply.github.com>
2025-06-05 13:53:38 -0400

    Add case-study

---
+++ oo-design/case-study/car-rental-system/car-rental-doc.py
@@ -0,0 +1,23 @@
+'''
+Let's design a Car Rental System
+
+This is to be build to handle car ernting for a short period ofn time, ranging from a few hours to weeks. The system has many local branches, allowing the user to return
+the vehicle to different locations, and it's primarily located near airports or general busy city areas.
+
+SYSTEM REQUIREMENTS:
+    1. Support the renting of different vehicles, like:
+        Cars
+        Trucks
+        SUVs
+        Vans
+        Motorcycles
+
+    2. Vehicle Info
+        Unique barcode
+        DETAILS - Make, Model, Year
+        Parking stall number for location
+    
+    3. Tracking Rentals
+        The system should show which member rented which vehicle
+        It shonuld also list all vehicles rented by a specific number.
+'''
